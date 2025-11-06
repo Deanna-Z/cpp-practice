1daffc21f6f8ce63ad44f02185859f6f315abd0d
Yaqing Zhao <deanna0517@hotmail.com>
2025-11-05 22:26:48 -0500

    add A+ B question

---
+++ 1.A+B/1.A+B.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+    int a, b;
+    while (std::cin >> a >> b)
+    {
+
+        int sum;
+        sum = a + b;
+        std::cout << sum << std::endl;
+
+    }
+
+    return 0;
+
+}
